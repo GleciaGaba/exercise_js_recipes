7c1db88542e0df50d7d9f4876eab7a5994b48c9f
GleciaGaba <gleciasf@hotmail.com>
2024-10-23 16:58:50 +0200

    first commit

---
+++ index/search.js
@@ -0,0 +1,67 @@
+const form = document.getElementsByTagName("form");
+const button = document.querySelector(".btn");
+
+const inputFn = () => {
+  let searchData = document.querySelector("#input").value;
+  console.log(searchData);
+  if (!searchData) {
+    // alert("Recipe not found.");
+    return;
+  }
+
+  filter(searchData);
+};
+
+async function filter(input) {
+  console.log(input);
+
+  try {
+    const response = await fetch(
+      `https://api.spoonacular.com/food/search?query=${input}&apiKey=43b1af8bb4924e2389e95aebca1724cb`
+    );
+    const data = await response.json();
+    //console.log(data);
+
+    const ingredients = data?.searchResults[0].results;
+
+    console.log("ingredients", ingredients);
+
+    if (!ingredients || ingredients.length === 0) {
+      //alert("No recipes found.");
+      return;
+    }
+
+    let cards = "";
+    for (const ingredient of ingredients) {
+      cards += `<div class="col">
+                    <div class="card h-100">
+                      <a href="${ingredient.link}" target="_blank">
+                        <img src="${ingredient.image}" class="card-img-top" alt="${ingredient.name}">
+                      </a>
+                      <div class="card-body">
+                        <h5 class="card-title">${ingredient.name}</h5>
+                      </div>
+                    </div>
+                  </div>`;
+    }
+
+    let div = document.querySelector("#cards");
+    div.innerHTML = cards;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+//button.addEventListener("submit", (event) => {
+//  event.preventDefault();
+//  let searchData = document.querySelector("#input").value;
+//  console.log(searchData);
+//  if (!searchData) {
+//    alert("Recipe not found.");
+//    return;
+//  }
+//
+//  filter(searchData);
+//});
+
+document.addEventListener("load", filter());
